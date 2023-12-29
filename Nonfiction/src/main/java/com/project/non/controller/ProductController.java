41616116e5b3913917d0576dac001b51004e1990
JAVA <JAVA@DESKTOP-AG705RJ>
2023-12-29 15:59:53 +0900

    ge

--- Nonfiction/src/main/java/com/project/non/controller/ProductController.java
+++ Nonfiction/src/main/java/com/project/non/controller/ProductController.java
@@ -10,7 +10,7 @@ public class ProductController {
 	@GetMapping("/")
 	public ModelAndView main() {
 		ModelAndView mav = new ModelAndView();
-		//ㅎㅇqq
+		//홍승희
 		mav.setViewName("index");
 		return mav;
 	}
