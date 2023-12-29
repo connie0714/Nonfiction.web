cbac885fc88fbe1b47535a86e4e151262f014624
JAVA <JAVA@DESKTOP-582MVDI>
2023-12-29 15:47:15 +0900

    테스트

--- Nonfiction/src/main/java/com/project/non/controller/ProductController.java
+++ Nonfiction/src/main/java/com/project/non/controller/ProductController.java
@@ -10,7 +10,7 @@ public class ProductController {
 	@GetMapping("/")
 	public ModelAndView main() {
 		ModelAndView mav = new ModelAndView();
-		
+		// 안녕하세요
 		mav.setViewName("index");
 		return mav;
 	}
