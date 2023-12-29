29d379eb93bd58e9926a6423b6101e28b3a70504
JAVA <JAVA@DESKTOP-AG705RJ>
2023-12-29 15:57:40 +0900

    aa

--- Nonfiction/src/main/java/com/project/non/controller/ProductController.java
+++ Nonfiction/src/main/java/com/project/non/controller/ProductController.java
@@ -10,7 +10,7 @@ public class ProductController {
 	@GetMapping("/")
 	public ModelAndView main() {
 		ModelAndView mav = new ModelAndView();
-		//ㅎㅇ1232
+		//ㅎㅇzz
 		mav.setViewName("index");
 		return mav;
 	}
