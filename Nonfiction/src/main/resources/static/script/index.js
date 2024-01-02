809593b67b4f95c23d306943f807e524f97f2afa
connie <50786998+connie0714@users.noreply.github.com>
2024-01-02 11:08:14 +0900

    Delete Nonfiction directory

--- Nonfiction/src/main/resources/static/script/index.js
+++
@@ -1,8 +0,0 @@
-
-
-window.onload = function() {
-    var video = document.querySelector(".fullscreen-video video");
-    video.style.width = "100%";
-    video.style.height = "100%";
-    video.style.objectFit = "fill";
-};
