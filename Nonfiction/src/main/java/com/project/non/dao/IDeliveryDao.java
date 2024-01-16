b3cae3ed7613c55a9440f17c94623b44db8d068c
connie <50786998+connie0714@users.noreply.github.com>
2024-01-16 11:15:43 +0900

    Delete Nonfiction directory
    reset

--- Nonfiction/src/main/java/com/project/non/dao/IDeliveryDao.java
+++
@@ -1,8 +0,0 @@
-package com.project.non.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface IDeliveryDao {
-
-}
--- Nonfiction/src/main/java/com/project/non/dto/AnswerVO.java
+++
@@ -1,9 +0,0 @@
-package com.project.non.dto;
-
-import lombok.Data;
-
-@Data
-public class AnswerVO {
-	private String content;
-	private String kind;
-}
